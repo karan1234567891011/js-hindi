870e2c8174c64383fc8ecb02712aa77c13310803
karan1234567891011 <karanMaitRR@gmail.com>
2025-03-14 20:49:22 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1253
+let accountEmail = "iuhhbjiv@gmail.com"
+var accPassword =  "111123"
+accCity = "jaipur"
+
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail ="yyuhnly@gmail.com"
+accPassword="4949494"
+accCity = "delhi"
+
+console.table([accountId, accountEmail, accPassword, accCity])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3+3) will not work bcz we are using node, not browser 
+
+let age=125
+
+console.log(typeof age);
+console.log( typeof null) // object
+console.log(typeof undefined) // undefined 
+
+
+
+
+
+ 
